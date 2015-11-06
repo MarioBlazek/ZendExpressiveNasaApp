c24335671c938b31b4ff3c55bfc8e5dc59cdcd6e
Mario Blazek <blazek.mario@gmail.com>
2015-11-06 18:15:37 +0100

    Basic dependencies and routes set

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -5,19 +5,16 @@ return [
         'invokables' => [
             Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\FastRouteRouter::class,
         ],
+        'factories' => [
+            App\Action\IndexAction::class => App\Action\IndexFactory::class,
+        ],
     ],
 
     'routes' => [
         [
-            'name' => 'home',
+            'name' => 'index',
             'path' => '/',
-            'middleware' => App\Action\HomePageAction::class,
-            'allowed_methods' => ['GET'],
-        ],
-        [
-            'name' => 'api.ping',
-            'path' => '/api/ping',
-            'middleware' => App\Action\PingAction::class,
+            'middleware' => App\Action\IndexAction::class,
             'allowed_methods' => ['GET'],
         ],
     ],
