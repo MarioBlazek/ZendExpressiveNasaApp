cb28b10fa05589b83594e5284d036242fe7b6f4d
Mario Blazek <blazek.mario@gmail.com>
2015-11-06 19:23:26 +0100

    IndexAction and IndexFactory set up

---
+++ src/Action/IndexAction.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Action;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Zend\Expressive\Template\TemplateRendererInterface;
+use Zend\Stratigility\MiddlewareInterface;
+
+/**
+ * Class IndexAction
+ * @package App\Action
+ */
+class IndexAction implements MiddlewareInterface
+{
+    private $templateRenderer;
+
+    public function __construct(TemplateRendererInterface $templateRenderer)
+    {
+        $this->templateRenderer = $templateRenderer;
+    }
+
+    /**
+     * Process an incoming request and/or response.
+     *
+     * Accepts a server-side request and a response instance, and does
+     * something with them.
+     *
+     * If the response is not complete and/or further processing would not
+     * interfere with the work done in the middleware, or if the middleware
+     * wants to delegate to another process, it can use the `$out` callable
+     * if present.
+     *
+     * If the middleware does not return a value, execution of the current
+     * request is considered complete, and the response instance provided will
+     * be considered the response to return.
+     *
+     * Alternately, the middleware may return a response instance.
+     *
+     * Often, middleware will `return $out();`, with the assumption that a
+     * later middleware will return a response.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param null|callable $out
+     * @return null|Response
+     */
+    public function __invoke(Request $request, Response $response, callable $out = null)
+    {
+        $html = $this->templateRenderer->render('app::index');
+        $response->getBody()->write($html);
+
+        return $response->withHeader('Content-Type', 'text/html');
+    }
+}
